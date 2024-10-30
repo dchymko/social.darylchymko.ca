7cd3dc1decf4da7ab89db03ed1dbac1a4c9eddd7
Daryl Chymko <dchymko@infometrix.ca>
2024-10-30 05:35:07 +0000

    Add Search

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::statamic('posts/type/{type}', 'type')->name('posts.type');
 Route::statamic('sitemap.xml', 'sitemap', ['content_type' => 'text/xml'])->name('sitemap');
 Route::statamic('feed.xml', 'feed', ['content_type' => 'text/xml'])->name('feed');
+Route::statamic('search', 'search_results')->name('search');
+
